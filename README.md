37ebb50ae146b434a7477d4fbdee2825abd53b99
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2023-01-26 23:40:40 +0700

    Refactor code and write Readme.md

---
+++ README.md
@@ -0,0 +1,9 @@
+การบ้านสำหรับการสมัครฝึกงาน Front end Developer ของ Dek-D Interactive co., ltd.
+# Tech stack
+HTML,CSS,Javascript
+
+# Feature
+- สามารถกดปุ่มซ้ายขวาเพื่อเลื่อน Slider ไปตามทิศทางที่ต้องการได้
+- สามารถกดที่จุดด้านล่างเพื่อไปหน้า Slider ที่ต้องการได้
+- แสดงหน้าที่ Slider Active เพียงหน้าเดียว
+- จุดด้านล่างมีการ Transition ระหว่างการกดเปลี่ยน
---
+++ index.js
@@ -0,0 +1,41 @@
+let current_slide = 0;
+let N = 5;
+
+const primaryColor = "#f37a01"
+
+const boxNav = document.getElementById("boxNav")
+const slide = document.getElementById("slide")
+
+//initialize
+for(let n=0;n<N;n++){
+    const img = document.createElement("img")
+    img.src = `./images/wallpaper${n+1}.jpg`
+    img.style.display = "none"
+    slide.appendChild(img)
+
+    const dot = document.createElement("div")
+    dot.className = "dot"
+    dot.onclick = function() { changeSlide(n) }
+    boxNav.appendChild(dot)
+}
+slide.childNodes[0].style.display = 'inline'
+boxNav.childNodes[current_slide].style.backgroundColor = primaryColor
+
+
+
+function changeSlide(to_slide_number) {
+    boxNav.childNodes[current_slide].style.backgroundColor = 'gray'
+    boxNav.childNodes[to_slide_number].style.backgroundColor = primaryColor
+    
+    slide.childNodes[current_slide].style.display = 'none'
+    slide.childNodes[to_slide_number].style.display = 'inline'
+    
+    current_slide = to_slide_number
+}
+
+function prevSlide(){
+    changeSlide(current_slide == 0 ? N-1 : current_slide - 1)
+}
+function nextSlide(){
+    changeSlide(current_slide == N-1 ? 0 : current_slide + 1)
+}
